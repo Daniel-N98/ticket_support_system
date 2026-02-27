27177becbb5634e3c8755bd4d1378c4c45eb1218
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 21:39:10 +0000

    Added notification indication to notifications & inbox.

--- src/components/header/buttons/InboxButton.tsx
+++ src/components/header/buttons/InboxButton.tsx
@@ -3,11 +3,14 @@
 import { Mail } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-export default function InboxButton({ iconClasses }: { iconClasses: string }) {
+export default function InboxButton({ iconClasses, hasNotification = false }: { iconClasses: string, hasNotification?: boolean }) {
   const router = useRouter();
   return (
-    <button className={`group rounded-lg ${iconClasses} w-10 h-10`} aria-label="Inbox" onClick={() => router.push("/dashboard/inbox")}>
+    <button className={`group relative w-10 h-10 p-0 rounded-full flex items-center justify-center bg-gray-800 hover:bg-gray-700 transition-colors ${iconClasses}`} aria-label="Inbox" onClick={() => router.push("/dashboard/inbox")}>
       <Mail size="24" className="text-white group-hover:text-blue-400 transition-colors" />
+      {hasNotification && (
+        <span className="absolute top-2 right-2 block w-1.5 h-1.5 rounded-full bg-red-500" />
+      )}
     </button>
   )
 }
--- src/components/header/buttons/NotificationButton.tsx
+++ src/components/header/buttons/NotificationButton.tsx
@@ -1,10 +1,14 @@
 import { Bell } from "lucide-react";
 
-export default function NotificationButton({ iconClasses }: { iconClasses: string }) {
+export default function NotificationButton({ iconClasses, hasNotification = false }: { iconClasses: string, hasNotification?: boolean }) {
 
   return (
-    <button className={`group rounded-lg ${iconClasses} w-10 h-10`} aria-label="Notifications">
-      <Bell size="24" className="text-white group-hover:text-blue-400 transition-colors" />
+    <button className={`group relative w-10 h-10 p-0 rounded-full flex items-center justify-center bg-gray-800 hover:bg-gray-700 transition-colors ${iconClasses}`} aria-label="Notifications">
+      <Bell size={24} className="text-white group-hover:text-blue-400 transition-colors" />
+
+      {hasNotification && (
+        <span className="absolute top-2 right-2 block w-1.5 h-1.5 rounded-full bg-red-500" />
+      )}
     </button>
   )
 }
