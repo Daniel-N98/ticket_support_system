dda3790fa3f80383226d0c988030b2662eaaf1fa
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 22:08:35 +0000

    Temp redirect for users not logged in.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header/Header";
 import NavBar from "@/components/nav-bar/NavBar";
+import { redirect } from "next/navigation";
 
 export default async function Layout({
   children,
@@ -7,6 +8,11 @@ export default async function Layout({
   children: React.ReactNode;
 }>) {
 
+  // Check if the user is logged in. If they are not, redirect to /login.
+  // If a user is not signed in, redirect to /login
+  const signedIn: boolean = false;
+  if (!signedIn) redirect("/login");
+  
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-main-primary text-white/90">
       <NavBar />
