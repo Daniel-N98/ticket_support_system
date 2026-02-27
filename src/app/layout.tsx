c691f396830f677b73a6db2126ce9994d9f02dad
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 04:00:12 +0000

    Added nav bar to main app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import NavBar from "@/components/nav-bar/NavBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,11 +12,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`flex flex-col md:flex-row min-h-screen bg-main-primary text-white/90`}
       >
+        <NavBar />
         {children}
       </body>
     </html>
