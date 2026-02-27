0afc84d911a608aae34dffe945f17808531b01bc
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 21:32:45 +0000

    Fixed aria-label for inbox and profile

--- src/components/header/buttons/InboxButton.tsx
+++ src/components/header/buttons/InboxButton.tsx
@@ -3,7 +3,7 @@ import { Mail } from "lucide-react";
 export default function InboxButton({ iconClasses }: { iconClasses: string }) {
 
   return (
-    <button className={`group rounded-lg ${iconClasses} w-10 h-10`} aria-label="Profile">
+    <button className={`group rounded-lg ${iconClasses} w-10 h-10`} aria-label="Inbox">
       <Mail size="24" className="text-white group-hover:text-blue-400 transition-colors" />
     </button>
   )
--- src/components/header/buttons/ProfileButton.tsx
+++ src/components/header/buttons/ProfileButton.tsx
@@ -3,7 +3,7 @@ import {  UserCircle2 } from "lucide-react";
 export default function ProfileButton({ iconClasses }: { iconClasses: string }) {
 
   return (
-    <button className={`group rounded-full ${iconClasses} w-12 h-12`} aria-label="Inbox">
+    <button className={`group rounded-full ${iconClasses} w-12 h-12`} aria-label="Profile">
       <UserCircle2 size="30" className="text-white group-hover:text-blue-400 transition-colors" />
     </button>
   )
