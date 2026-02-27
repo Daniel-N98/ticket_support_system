1cd9f105b2f273c3d62241f040a0006e8a359aff
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 20:56:46 +0000

    Improve NavLink > Active page is now shown for both md and sm screens.

--- src/components/nav-bar/NavLink.tsx
+++ src/components/nav-bar/NavLink.tsx
@@ -1,16 +1,23 @@
-import {  VisibleNavLinkType } from "@/types/Nav";
+import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 
-export default function NavLink({ label, icon, href }: VisibleNavLinkType) {
+interface NavLinkProps {
+  label: string;
+  icon: React.ReactNode;
+  href: string;
+}
+
+export default function NavLink({ label, icon, href }: NavLinkProps) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
 
   return (
     <Button
-      key={label}
       variant="ghost"
-      className="flex items-center gap-4 justify-center md:justify-start text-md tracking-wide text-white/50 hover:bg-zinc-700 hover:text-white"
+      className={`${isActive ? "bg-zinc-700 md:bg-inherit text-white font-semibold" : "text-white/50"} hover:bg-zinc-700 hover:text-white w-full flex items-center gap-4 justify-center md:justify-start text-base tracking-wide`}
       asChild
     >
-      <a href={href} className="flex items-center gap-2 w-full">
+      <a href={href} className="flex items-center gap-2 w-full font-bold" aria-current={isActive ? "page" : undefined}>
         {icon}
         <span className="hidden md:inline">{label}</span>
       </a>
