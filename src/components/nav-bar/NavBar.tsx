9292ce7e89eed85fce3908cf278ce3609fe5336e
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 04:07:06 +0000

    Adjusted spacing of nav for smaller screens

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -4,14 +4,14 @@ export default function NavBar() {
 
   return (
     <aside className="w-full md:w-64 bg-main-secondary px-6 py-4 md:py-9 flex flex-col border-b border-white/30 md:border-0">
-      <div className="flex flex-col mb-8 space-y-1 items-center md:items-start">
+      <div className="flex flex-col mb-4 md:mb-8 space-y-1 items-center md:items-start">
         <h1>
           <a href="/dashboard" className="text-2xl font-bold text-zinc-50 md:pl-4">
             <span className="text-blue-400">Ticket</span>
             System</a>
         </h1>
       </div>
-      <nav className="flex flex-col justify-between h-full mt-2 md:mt-8">
+      <nav className="flex flex-col justify-between h-full md:mt-8">
         <NavLinks />
       </nav>
     </aside>
