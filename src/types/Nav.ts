960c2a6eca0f12bf7036e713387b9a763f5d43d1
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-27 20:37:45 +0000

    Added Role type, and NavLink types (With and without Roles)

--- src/types/Nav.ts
+++ src/types/Nav.ts
@@ -1,4 +1,13 @@
+import { Role } from "./Roles";
+
 export type NavLinkType = {
+  label: string;
+  icon: React.ReactNode;
+  href: string;
+  roles: Role[];
+}
+
+export interface VisibleNavLinkType {
   label: string;
   icon: React.ReactNode;
   href: string;
