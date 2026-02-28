a8dccbc27e4d6c2b38f48d44db7a9a43df58c169
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-28 22:46:12 +0000

    Base new ticket page

---
+++ src/app/dashboard/tickets/new/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState } from "react";
+import { AlertCircle } from "lucide-react";
+import NewTicketForm from "@/components/tickets/new/NewTicketForm";
+import NewTicketHeader from "@/components/tickets/new/NewTicketHeader";
+
+
+export default function NewTicketPage() {
+  const [loading, setIsLoading] = useState<boolean>(false);
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData(e.currentTarget);
+    const subject = formData.get("subject") as string;
+    const content = formData.get("content") as string;
+    const priority = formData.get("priority") as string;
+    if (!subject || !content || !priority) {
+      return;
+    }
+
+    
+  };
+
+  return (
+    <div>
+      <div className="h-12" />
+
+      <section className="w-full min-h-[187.5px] bg-main-secondary mt-9 rounded-lg flex justify-center">
+        <div className="w-full max-w-3xl p-8 md:p-10">
+
+          {/* Header Section */}
+          <NewTicketHeader />
+          <NewTicketForm handleSubmit={handleSubmit} loading={loading} />
+
+          <div className="w-max py-3 p-6 mt-10 flex items-center gap-2 text-white/50 text-xs bg-white/5 rounded-md">
+            <AlertCircle className="w-4 h-4" />
+            <span>Average response time: &lt; 24 hours</span>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
