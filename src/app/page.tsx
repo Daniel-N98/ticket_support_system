d2f70bcf592cba210bd667ad7baede7552e6d88d
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-28 00:47:15 +0000

    Removed signedIn check from app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  // If a user is not signed in, redirect to /login
-  const signedIn: boolean = false;
-  redirect(signedIn ? "/dashboard" : "/login");
+  redirect("/dashboard");
 }
