906f53dc950b5a8d9cf698c08877c9206c56cf70
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-28 01:55:23 +0000

    Styling change for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import HeaderButtons from "./buttons/HeaderButtons";
 export default function Header() {
 
   return (
-    <header className="flex justify-between items-center px-2 md:px-6 -mt-3">
+    <header className="flex justify-between items-center pr-2 md:pr-6 -mt-3">
       <BreadcrumbsHeader />
       <div className="hidden md:flex">
         <HeaderButtons />
