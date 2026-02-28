62d4905192f1edd38fb6185f28ec449f91c3f930
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-02-28 22:46:46 +0000

    Adjusted search bar height so things align properly.

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -4,7 +4,7 @@ import { Input } from "./input";
 export default function SearchBar({ placeholder }: { placeholder: string }) {
 
   return (
-    <div className="relative w-full lg:w-full xl:w-1/4 mt-3">
+    <div className="relative w-full lg:w-full xl:w-1/4 mt-3 h-12">
       <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-white pointer-events-none" size={18} />
 
       <Input
