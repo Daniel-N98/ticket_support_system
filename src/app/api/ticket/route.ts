3d6d06df8734746a5425106494832d4a3bdfdf65
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 00:47:00 +0000

    GET tickets route

--- src/app/api/ticket/route.ts
+++ src/app/api/ticket/route.ts
@@ -2,34 +2,19 @@ import dbConnect from "@/lib/mongodb";
 import Ticket from "@/models/Ticket";
 import User from "@/models/User";
 import { CreatedTicket } from "@/types/Ticket";
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { authOptions } from "../auth/[...nextauth]/route";
-import { hasPermission } from "@/utils/hasPermission";
+import { hasPermission, requirePermission, requireSession } from "@/lib/permissionUtils";
 import { PERMISSIONS } from "@/types/Permissions";
 
 export async function GET() {
-  await dbConnect()
+  await dbConnect();
 
   try {
-    const session = await getServerSession(authOptions);
-    if (!session) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
+    const session = await requireSession(); // Require session to access this route.
+    const canViewAll = await hasPermission(PERMISSIONS.TICKETS_ALL_VIEW);
 
-    let tickets;
-    if (hasPermission(session.user.role, PERMISSIONS.TICKETS_ALL_VIEW)) {
-      // Return all tickets.
-      tickets = await Ticket.find({})
-        .populate("customer", "email image name")
-        .lean();
-    } else {
-      // Only return this user's tickets.
-      tickets = await Ticket.find({ customer: session.user.id })
-        .populate("customer", "email image name")
-        .lean();
-    }
-
+    const query = canViewAll ? {} : { customer: session.user.id };
+    const tickets = await Ticket.find(query).populate("customer", "email image name").lean();
 
     const formattedTickets = tickets.map(({ customer, ...ticket }) => ({
       ...ticket,
@@ -38,39 +23,38 @@ export async function GET() {
       customerImage: customer?.image ?? null,
     }));
 
-    return NextResponse.json(
-      { success: true, tickets: formattedTickets },
-      { status: 200 }
-    )
+    return NextResponse.json({ success: true, tickets: formattedTickets }, { status: 200 });
   } catch (error) {
-    return NextResponse.json(
-      { success: false, error },
-      { status: 500 }
-    )
+    return NextResponse.json({ success: false, error }, { status: 500 });
   }
 }
 
 export async function POST(req: NextRequest) {
-  const { customerId, subject, content, priority }: CreatedTicket = await req.json();
+  const { subject, content, priority }: CreatedTicket = await req.json();
 
-  if (!customerId || !subject || !content || !priority) {
+  if (!subject || !content || !priority) {
     return NextResponse.json({ error: "Missing fields" }, { status: 400 });
   }
 
   try {
+    const session = await requireSession();
+    if (!await requirePermission(PERMISSIONS.TICKETS_CREATE)) {
+      return NextResponse.json({ error: "Unauthorized" }, {status: 403});
+    }
+
     await dbConnect();
     // Check user exists.
-    const userExists = await User.exists({ _id: customerId });
+    const userId = session.user.id;
+    const userExists = await User.exists({ _id: userId });
     if (!userExists) {
       return NextResponse.json({ error: "User does not exist." }, { status: 400 });
     }
     // Create ticket
-    const ticket = await Ticket.create({ customer: customerId, subject, content, priority });
+    const ticket = await Ticket.create({ customer: userId, subject, content, priority });
 
     return NextResponse.json({ message: "Ticket successfully created.", ticket });
   } catch (error) {
     console.log(error);
-
     return NextResponse.json({ error: "Ticket could not be created." }, { status: 500 });
   }
 }
