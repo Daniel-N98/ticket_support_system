25e87b9b824578479207ab0549ba58da499fb7cc
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 02:32:26 +0000

    Loading ticket on ticket page

--- src/components/tickets/ticket/Ticket.tsx
+++ src/components/tickets/ticket/Ticket.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { deleteTicketByTicketId } from "@/lib/api/ticket.api";
+import { deleteTicketByTicketId, fetchTicketByTicketId } from "@/lib/api/ticket.api";
+import { TicketType } from "@/types/Ticket";
 import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function Ticket() {
+  const [ticket, setTicket] = useState<TicketType | null>(null);
+
   const { ticketId } = useParams();
   const router = useRouter();
+
+  useEffect(() => {
+    async function loadTicket() {
+      const ticketResponse: TicketType | null = await fetchTicketByTicketId(ticketId as string);
+      if (ticketResponse) {
+        setTicket(ticketResponse);
+      }
+    }
+    loadTicket();
+  }, []);
 
   async function deleteTicket() {
     const deleteResponse: string | null = await deleteTicketByTicketId(ticketId as string);
