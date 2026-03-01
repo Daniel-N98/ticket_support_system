e2f780118c90c3a179437df2acd1f4958aba0fa4
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 17:43:21 +0000

    Actually passing session to hasPermission if it is provided.

--- src/lib/permissionUtils.ts
+++ src/lib/permissionUtils.ts
@@ -2,13 +2,13 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Permission, ROLE_PERMISSIONS } from "@/types/Role";
 import { getServerSession, Session } from "next-auth";
 
-export async function hasPermission(permission: Permission, session?: Session) {
+export async function hasPermission(permission: Permission, session?: Session | null) {
   const serverSession = session ?? await requireSession();
   return ROLE_PERMISSIONS[serverSession.user.role]?.includes(permission) ?? false;
 }
 
 export async function requirePermission(permission: string, session?: Session) {
-  return await hasPermission(permission);
+  return await hasPermission(permission, session || null);
 }
 
 export async function requireSession() {
