3537085893717270d356d25ad9acfa80df75746a
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 19:49:32 +0000

    Wrapping text in subject to prevent it overflowing the box.

--- src/components/tickets/ticket/TicketContent.tsx
+++ src/components/tickets/ticket/TicketContent.tsx
@@ -7,7 +7,7 @@ export default function TicketContent({ ticket }: { ticket: TicketType }) {
     <Card className="bg-main-secondary border-white/10 text-white min-h-96 flex flex-col">
       <CardHeader className="border-b border-white/5 pb-6">
         <span className="text-white/40 text-sm mb-2">Ticket ID: {ticket.ticketId}</span>
-        <h1 className="text-3xl font-bold tracking-tight">{ticket.subject}</h1>
+        <h1 className="text-3xl font-bold tracking-tight wrap-break-word">{ticket.subject}</h1>
       </CardHeader>
       <CardContent className="pt-6 flex-1">
         <div
