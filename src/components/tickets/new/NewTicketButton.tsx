e8ca45c7b6733122aafa993626c5ab9e09ce2572
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 23:07:25 +0000

    New ticket button is now just a +

--- src/components/tickets/new/NewTicketButton.tsx
+++ src/components/tickets/new/NewTicketButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { PlusSquare } from "lucide-react";
 import Link from "next/link";
 
 export default function NewTicketButton() {
@@ -6,7 +7,7 @@ export default function NewTicketButton() {
   return (
     <Link href={"/dashboard/tickets/new"}>
       <Button className="w-max bg-blue-500 hover:bg-blue-600 mt-3 hover:cursor-pointer">
-        Create Ticket
+        <PlusSquare />
       </Button>
     </Link>
   )
