1200058d9ca674a6f2959641fba4c8c100024bb7
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 19:46:45 +0000

    Added min height to ticket content

--- src/components/tickets/ticket/TicketContent.tsx
+++ src/components/tickets/ticket/TicketContent.tsx
@@ -4,7 +4,7 @@ import { TicketType } from "@/types/Ticket";
 export default function TicketContent({ ticket }: { ticket: TicketType }) {
 
   return (
-    <Card className="bg-main-secondary border-white/10 text-white h-full flex flex-col">
+    <Card className="bg-main-secondary border-white/10 text-white min-h-96 flex flex-col">
       <CardHeader className="border-b border-white/5 pb-6">
         <span className="text-white/40 text-sm mb-2">Ticket ID: {ticket.ticketId}</span>
         <h1 className="text-3xl font-bold tracking-tight">{ticket.subject}</h1>
