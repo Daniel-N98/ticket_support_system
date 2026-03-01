aca2e953faf688e758e516188bd9a904509679c8
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 22:33:30 +0000

    Api call now returns updated ticket

--- src/lib/api/ticketReply.api.ts
+++ src/lib/api/ticketReply.api.ts
@@ -1,6 +1,7 @@
 import { TicketReplyType, TicketReplyTypeCrate } from "@/types/TicketReply";
 import apiClient from "../api";
 import toast from "react-hot-toast";
+import { TicketType } from "@/types/Ticket";
 
 export async function fetchTicketReplies(ticketId: string): Promise<TicketReplyType[] | null> {
   try {
@@ -11,11 +12,11 @@ export async function fetchTicketReplies(ticketId: string): Promise<TicketReplyT
   }
 }
 
-export async function postTicketReply({ ticketId, content }: TicketReplyTypeCrate): Promise<TicketReplyType | null> {
+export async function postTicketReply({ ticketId, content }: TicketReplyTypeCrate): Promise<{ ticketReply: TicketReplyType, ticket: TicketType } | null> {
   try {
-    const response: { message: string, ticketReply: TicketReplyType } = await apiClient.post("/ticket/reply", { ticketVisibleId: ticketId, content });
+    const response: { message: string, ticketReply: TicketReplyType, updatedTicket: TicketType } = await apiClient.post("/ticket/reply", { ticketVisibleId: ticketId, content });
     toast.success(response.message);
-    return response.ticketReply;
+    return { ticketReply: response.ticketReply, ticket: response.updatedTicket };
   } catch (error) {
     // @ts-expect-error Random
     if (error.response?.data?.error) {
