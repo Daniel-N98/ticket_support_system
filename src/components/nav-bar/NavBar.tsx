981f51ab47c3f09cfca40851355dff23027d8300
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 00:14:11 +0000

    Typo

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -4,7 +4,7 @@ import NavLinks from "./NavLinks";
 export default function NavBar() {
 
   return (
-    <aside className="w-full md:w-64 bg-main-secondary px-6 py-4 md:py-9 flex flex-col border-b border-white/30 md:border-0 md:h-screen md:fixed md:left-0 md:top0">
+    <aside className="w-full md:w-64 bg-main-secondary px-6 py-4 md:py-9 flex flex-col border-b border-white/30 md:border-0 md:h-screen md:fixed md:left-0 md:top-0">
       <div className="flex justify-between mb-4 md:mb-8 space-y-1 items-center md:items-start">
         <h1>
           <a href="/dashboard" className="text-2xl font-bold text-zinc-50 md:pl-4">
