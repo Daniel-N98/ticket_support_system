89e89727d0457582571bdde6f671d22e60f1de7a
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 00:45:55 +0000

    Removed old hasPermissions file

--- src/utils/hasPermission.ts
+++
@@ -1,8 +0,0 @@
-import { Permission, ROLE_PERMISSIONS } from "@/types/Role";
-
-export function hasPermission(
-  role: string,
-  permission: Permission
-) {
-  return ROLE_PERMISSIONS[role]?.includes(permission) ?? false;
-}
