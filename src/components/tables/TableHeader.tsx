801d2e8721e14ce82900ea5391df86f259e3ca0c
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 18:27:19 +0000

    Table header widths set based on column

--- src/components/tables/TableHeader.tsx
+++ src/components/tables/TableHeader.tsx
@@ -1,12 +1,22 @@
 import { TableHead, TableHeader, TableRow } from "../ui/table";
 
-export default function TableHeaders({rows}: {rows: string[]}) {
+export default function TableHeaders({ rows }: { rows: string[] }) {
+
+  const getWidth = (row: string) => {
+    switch (row) {
+      case "Ticket #": return "w-22";
+      case "Status": return "w-22";
+      case "Priority": return "w-22";
+      case "Agent": return "w-22";
+      default: return "w-32";
+    }
+  }
 
   return (
     <TableHeader>
       <TableRow className="hover:bg-inherit">
         {rows.map((row: string) => (
-          <TableHead key={row} className="text-white/90">{row}</TableHead>
+          <TableHead key={row} className={`${getWidth(row)} text-white/90`}>{row}</TableHead>
         ))}
       </TableRow>
     </TableHeader>
