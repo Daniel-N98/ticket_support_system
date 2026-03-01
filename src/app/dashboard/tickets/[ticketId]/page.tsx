0157c6753e36a1c54e7f45883ceecff9e36074d2
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-01 22:25:55 +0000

    Removed duplicate margin

--- src/app/dashboard/tickets/[ticketId]/page.tsx
+++ src/app/dashboard/tickets/[ticketId]/page.tsx
@@ -5,7 +5,7 @@ export default function TicketPage() {
   return <div>
 
     <div className="h-12" />
-    <section className="w-full min-h-187.5 bg-main-secondary mt-6 rounded-lg mt-9">
+    <section className="w-full min-h-187.5 bg-main-secondary rounded-lg mt-9">
       <Ticket />
     </section>
   </div>
