dc4d780406fd0103e719a80655abfd66a9694816
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-02 00:34:54 +0000

    Inbox header created

---
+++ src/components/inbox/table/InboxHeader.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import SearchBar from "@/components/ui/SearchBar";
+
+export default function InboxHeader() {
+
+  return (
+    <SearchBar placeholder="Search by customer name." />
+  )
+}
