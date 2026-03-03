1bd5d61e4f0ea56786d02bc5f72861af08ed966a
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2026-03-03 22:17:14 +0000

    Layout now displays a banner at the top when website is in lockdown mode.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,7 +14,8 @@ export default async function Layout({
   // Check if the user is logged in. If they are not, redirect to /login.
   // If a user is not signed in, redirect to /login
   const session = await getServerSession(authOptions);
-  await loadSiteSettings(); // Load site-settings.
+  const settings = await loadSiteSettings(); // Load site-settings.
+  const isInLockdown = settings.every((setting) => setting.value === false);
 
   if (!session) {
     redirect("/login");
@@ -24,6 +25,11 @@ export default async function Layout({
 
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-main-primary text-white/90">
+      {isInLockdown && (
+        <div className="h-17 md:h-10 w-full bg-main-secondary fixed top-0 left-0 z-50">
+          <p className="text-white text-center mt-2">The site is currently in <span className="text-red-500">lockdown mode</span>. All features are disabled. Please check back later.</p>
+        </div>
+      )}
       <NavBar session={session} />
       <div className="py-8 px-3 md:py-11 md:px-12 w-full md:ml-64">
         <Header />
